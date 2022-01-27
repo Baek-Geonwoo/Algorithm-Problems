d08d10d3214b544140190da16b9f92d7ce12468a
Baek-GeonWoo <phlox3959@gmail.com>
2022-01-27 22:00:43 +0900

    Edit 1931.py

--- 1931/1931.py
+++ 1931/1931.py
@@ -2,7 +2,7 @@ import sys
 I = sys.stdin.readline
 N = int(I())
 A = [list(map(int,I().split())) for _ in range(N)]
-A.sort(key = lambda x: [x[1],x[0]])
+A.sort(key = lambda x: [x[1], x[0]])
 curr_time = 0
 cnt = 0
 for s,e in A:
@@ -10,4 +10,3 @@ for s,e in A:
 		curr_time = e
 		cnt += 1
 print(cnt)
-print(A)
