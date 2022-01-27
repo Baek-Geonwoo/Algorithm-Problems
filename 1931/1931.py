b463b52a64bb63e6312e1b9f2b145e4480b2b255
Baek-GeonWoo <phlox3959@gmail.com>
2022-01-27 21:27:53 +0900

    Solve BOJ 1931

---
+++ 1931/1931.py
@@ -0,0 +1,13 @@
+import sys
+I = sys.stdin.readline
+N = int(I())
+A = [list(map(int,I().split())) for _ in range(N)]
+A.sort(key = lambda x: [x[1],x[0]])
+curr_time = 0
+cnt = 0
+for s,e in A:
+	if curr_time <= s:
+		curr_time = e
+		cnt += 1
+print(cnt)
+print(A)
