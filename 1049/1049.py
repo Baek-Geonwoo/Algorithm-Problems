93be62cc965bdb5a6076a1090ec68f53b9a2a1f7
Baek-GeonWoo <phlox3959@gmail.com>
2022-02-07 13:48:48 +0900

    Fix 1049.py typo

--- 1049/1049.py
+++ 1049/1049.py
@@ -2,9 +2,8 @@ import sys
 I = sys.stdin.readline
 N, M = map(int, I().split())
 S = [list(map(int, I().split())) for _ in range(M)]
-Set, EA = 100*6, 100
-m = 1000*N
-for s, i in S:
+Set, EA = 1000*6, 1000
+for s, e in S:
     Set = min(Set, s)
-    EA = min(EA, i)
+    EA = min(EA, e)
 print(min(Set*(N//6+1), Set*(N//6) + EA*(N%6), EA*N))
