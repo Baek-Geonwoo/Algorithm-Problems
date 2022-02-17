90fe4bbbe52784b6930a84b354041d849dd7721a
Baek-GeonWoo <phlox3959@gmail.com>
2022-02-17 16:06:16 +0900

    Fix 21736.py typo

--- 21736/21736.py
+++ 21736/21736.py
@@ -7,29 +7,28 @@ def in_range(x,y):
     return False
 N, M = map(int, I().split())
 C = [I().strip() for _ in range(N)]
-visited = [[False]*M for _ in range(N)]
 stack = deque()
 for i in range(N):
     for j in range(M):
         if C[i][j] == 'I':
             stack.append((i,j))
-            break
+visited = [[False]*M for _ in range(N)]
 ans = 0
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 while stack:
     x, y = stack.pop()
-    visited[x][y] = True
-    if C[x][y] == 'X':
-        continue
-    if C[x][y] == 'P':
-        ans += 1
-    for i in range(4):
-        X = x+dx[i]
-        Y = y+dy[i]
-        if in_range(X,Y):
-            if visited[X][Y] == True: continue
-            stack.append((X,Y))
+    if not visited[x][y]:
+        visited[x][y] = True
+        if C[x][y] == 'X':
+            continue
+        if C[x][y] == 'P':
+            ans += 1
+        for i in range(4):
+            X = x+dx[i]
+            Y = y+dy[i]
+            if in_range(X,Y):
+                stack.append((X,Y))
 if not ans:
     print("TT")
 else:
