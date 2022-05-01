31dc7f3a24d99e13706138111d3a804137ee52d0
Baek-GeonWoo <phlox3959@gmail.com>
2022-05-02 03:45:20 +0900

    Update BOJ 1484

--- 1484/1484.py
+++ 1484/1484.py
@@ -3,6 +3,6 @@ G = int(sys.stdin.readline())
 flag = 1
 for n in range(int(G**0.5),0,-1):
     if G%n==0 and (n+G//n)%2 == 0 and n!=G//n:
-        print(n,G//n,(n+G//n)//2)
+        print((n+G//n)//2)
         flag = 0
 if flag: print(-1)
