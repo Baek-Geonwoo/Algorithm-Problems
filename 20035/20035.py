d3819dfaf6a955afff3869b39b39a7bd84a04051
Baek-GeonWoo <phlox3959@gmail.com>
2022-05-15 17:47:21 +0900

    Update BOJ 20035

--- 20035/20035.py
+++ 20035/20035.py
@@ -5,10 +5,6 @@ N, M = I()
 A = list(I())
 B = list(I())
 m = max(A)
-if A.count(m) == 1:
-    i = A.index(m)
-    print((sum(A)+m*(M-1))*10**9+sum(B)+B[0]*i+B[-1]*(N-1-i))
-else:
-    lo = A.index(m)
-    hi = list(reversed(A)).index(m)
-    print((sum(A)+m*(M-1))*10**9+sum(B)+B[0]*lo+max(B)*(hi-lo)+B[-1]*(N-1-hi))
+lo = A.index(m)
+hi = N-1-list(reversed(A)).index(m)
+print((sum(A)+m*(M-1))*10**9+sum(B)+B[0]*lo+max(B)*(hi-lo)+B[-1]*(N-1-hi))
